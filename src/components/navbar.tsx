0249974bed2d84d7d7bde4b68d0c5a7bf2a83043
tojoabella <terricabella@gmail.com>
2026-05-01 02:07:28 -1000

    refactored navbar into a list of menu items

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,62 +3,35 @@ import { useState } from "react";
 import HomePage from "./homepage";
 import AbstractPage from "./abstract";
 
-function NavBar() {
-  const [activePage, setActivePage] = useState("home");
-
+function NavBar({
+  activePage,
+  setActivePage,
+}: {
+  activePage: string;
+  setActivePage: (page: string) => void;
+}) {
+  const pages = ["home", "models", "birds", "abstract", "contact"];
   return (
     <>
       <nav className="bg-gray-800 p-4">
         <div className="container mx-auto flex justify-between items-center">
+          {/*first flexbox item */}
           <div className="text-white text-lg font-bold">
             Hawaii Bird Classification
           </div>
+          {/*second flexbox item */}
           <div>
-            <a
-              href="#"
-              className={`text-gray-300 hover:text-white mx-2 ${
-                activePage === "home" ? "text-white" : ""
-              }`}
-              onClick={() => setActivePage("home")}
-            >
-              Home
-            </a>
-            <a
-              href="#"
-              className={`text-gray-300 hover:text-white mx-2 ${
-                activePage === "models" ? "text-white" : ""
-              }`}
-              onClick={() => setActivePage("models")}
-            >
-              Models
-            </a>
-            <a
-              href="#"
-              className={`text-gray-300 hover:text-white mx-2 ${
-                activePage === "birds" ? "text-white" : ""
-              }`}
-              onClick={() => setActivePage("birds")}
-            >
-              Birds
-            </a>
-            <a
-              href="#"
-              className={`text-gray-300 hover:text-white mx-2 ${
-                activePage === "abstract" ? "text-white" : ""
-              }`}
-              onClick={() => setActivePage("abstract")}
-            >
-              Abstract
-            </a>
-            <a
-              href="#"
-              className={`text-gray-300 hover:text-white mx-2 ${
-                activePage === "contact" ? "text-white" : ""
-              }`}
-              onClick={() => setActivePage("contact")}
-            >
-              Contact
-            </a>
+            {pages.map((page) => (
+              <button
+                key={page}
+                className={`text-gray-300 hover:text-white mx-2 ${
+                  activePage === page ? "text-white" : ""
+                }`}
+                onClick={() => setActivePage(page)}
+              >
+                {page.charAt(0).toUpperCase() + page.slice(1)}
+              </button>
+            ))}
           </div>
         </div>
       </nav>
