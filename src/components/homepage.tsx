79a91ff4bb961af36722f64103f54249ed70c234
tojoabella <terricabella@gmail.com>
2026-04-30 22:47:16 -1000

    used prettier on homepage

--- src/components/homepage.tsx
+++ src/components/homepage.tsx
@@ -1,38 +1,52 @@
 function NavBar() {
-    return (
-        <nav className="bg-gray-800 p-4">
-            <div className="container mx-auto flex justify-between items-center">
-                <div className="text-white text-lg font-bold">Hawaii Bird Classification</div>
-                <div>
-                    <a href="#" className="text-gray-300 hover:text-white mx-2">Home</a>
-                    <a href="#" className="text-gray-300 hover:text-white mx-2">Models</a>
-                    <a href="#" className="text-gray-300 hover:text-white mx-2">Birds</a>
-                    <a href="#" className="text-gray-300 hover:text-white mx-2">Abstract</a>
-                    <a href="#" className="text-gray-300 hover:text-white mx-2">Contact</a>
-                </div>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white text-lg font-bold">
+          Hawaii Bird Classification
+        </div>
+        <div>
+          <a href="#" className="text-gray-300 hover:text-white mx-2">
+            Home
+          </a>
+          <a href="#" className="text-gray-300 hover:text-white mx-2">
+            Models
+          </a>
+          <a href="#" className="text-gray-300 hover:text-white mx-2">
+            Birds
+          </a>
+          <a href="#" className="text-gray-300 hover:text-white mx-2">
+            Abstract
+          </a>
+          <a href="#" className="text-gray-300 hover:text-white mx-2">
+            Contact
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 function ImagePlaceholder() {
-    return (
-        <div className="flex flex-col justify-center items-center w-128 h-64 bg-gray-300 rounded-lg">
-            <div className="">Image Placeholder</div>
-            <button className="mx-4 px-4 py-2 bg-blue-500 text-white hover:bg-blue-600 rounded">Path</button>
-        </div>
-    )
+  return (
+    <div className="flex flex-col justify-center items-center w-128 h-64 bg-gray-300 rounded-lg">
+      <div className="">Image Placeholder</div>
+      <button className="mx-4 px-4 py-2 bg-blue-500 text-white hover:bg-blue-600 rounded">
+        Path
+      </button>
+    </div>
+  );
 }
 
 function HomePage() {
-    return (
-        <div>
-            {/* <NavBar /> */}
-            <div className="flex justify-center items-center h-screen">
-                <ImagePlaceholder />
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <NavBar />
+      <div className="flex justify-center items-center h-screen">
+        <ImagePlaceholder />
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
