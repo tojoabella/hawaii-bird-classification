d3f09b63095e199d80fee89acfc03d3fe875b52d
tojoabella <terricabella@gmail.com>
2026-05-07 23:39:39 -1000

    link repo on contacts page

--- src/components/contacts.tsx
+++ src/components/contacts.tsx
@@ -5,8 +5,11 @@ function ContactsPage() {
         <h1>Contact</h1>
       </div>
       <div className="mx-4">Tojo Abella</div>
-      <div className="mx-4">https://github.com/tojoabella</div>
-    </>
+      <div className="mx-4">https://github.com/tojoabella/</div>
+      <div className="mx-4">
+        https://github.com/tojoabella/hawaii-bird-classification
+      </div>
+    </div>
   );
 }
 
